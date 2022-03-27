d317b3628e191eab540460f29d9f858f26bbdc26
enes_gundogan <53365066+nsgnd@users.noreply.github.com>
2022-03-27 15:48:07 +0300

    Delete week-3/2.package/2. start_with_lower_case directory

--- week-3/2.package/2. start_with_lower_case/formatter/formatter.go
+++
@@ -1,7 +0,0 @@
-package formatter
-
-import "fmt"
-
-func format(num int) string {
-	return fmt.Sprintf("The number is %d", num)
-}
--- week-3/2.package/2. start_with_lower_case/math/math.go
+++
@@ -1,7 +0,0 @@
-// package name
-package math
-
-// Double is public function because it starts with uppercase
-func double(a int) int {
-	return a * 2
-}
